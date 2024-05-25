d5db16a6fdf31b66515a0449573f9b7133f08bdb
MarJos1502 <bolvidermarcjoseph@gmail.com>
2024-05-26 00:53:30 +0800

    Latest Lesson

--- sectiona/models.py
+++ sectiona/models.py
@@ -11,13 +11,16 @@ class Gender(models.Model):
     class Meta:
        db_table = 'genders'
 
+    def __str__(self):
+        return self.gender
+
 class User(models.Model):
     user_id = models.BigAutoField(primary_key=True, blank=False)  # BIGINT NOT NULL AUTO INCREMENT PRIMARY KEY
     first_name = models.CharField(max_length=55, blank=False)  # VARCHAR(55) NOT NULL
     middle_name = models.CharField(max_length=55, blank=True)  # VARCHAR(55) NULL
     last_name = models.CharField(max_length=55, blank=False)  # VARCHAR(55) NOT NULL
     age = models.IntegerField(blank=False)  # INTEGER
-    birth_date = models.DateField(blank=False)  # DATE
+    birth_date = models.DateField(blank=True)  # DATE
     gender = models.ForeignKey(Gender, on_delete=models.CASCADE)  # Foreign key to Gender
     username = models.CharField(max_length=55, blank=False )  # VARCHAR(55) NOT NULL
     password = models.CharField(max_length=255, blank=False)  # VARCHAR(255) NOT NULL
--- sectiona/views.py
+++ sectiona/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Gender
+from .models import Gender, User
 from django.contrib import messages
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 
@@ -22,4 +23,83 @@ def store_gender(request):
     messages.success(request, 'Gender successfully saved!')
     return redirect('/genders')
 
+def show_gender(request, gender_id):
+    gender = Gender.objects.get(pk=gender_id)
 
+    context = {
+        'gender': gender,
+    }
+
+    return render(request, 'gender/show.html', context)
+
+def edit_gender(request, gender_id):
+    gender = Gender.objects.get(pk=gender_id)
+
+    context = {
+        'gender': gender,
+    }
+
+    return render(request, 'gender/edit.html', context)
+
+def update_gender(request, gender_id):
+    gender = request.POST.get('gender')
+    
+    Gender.objects.filter(pk=gender_id).update(gender=gender)
+    messages.success(request, 'Gender successfully update')
+
+    return redirect('/genders')
+
+def delete_gender(request, gender_id):
+    gender = Gender.objects.get(pk=gender_id)
+
+    context = {
+        'gender': gender,
+    }
+
+    return render(request, 'gender/delete.html', context)
+
+def destroy_gender(request, gender_id):
+    Gender.objects.filter(pk=gender_id).delete()
+    messages.success(request, 'Gender succesfully deleted')
+
+    return redirect('/genders')
+
+def index_user(request):
+    users = User.objects.all()  
+
+    context = {
+        'users': users
+    }
+    
+    return render(request, 'user/index.html', context)
+
+
+def create_user(request): 
+    genders  = Gender.objects.all()
+
+    context = {
+        'genders': genders
+    }
+    return render(request, 'user/create.html', context)
+
+def store_user(request):
+    firstName = request.POST.get('first_name')
+    midddleName = request.POST.get('middle_name')
+    lastName = request.POST.get('last_name')
+    age = request.POST.get('age')
+    birthDate = request.POST.get('birth_date')
+    genderId = request.POST.get('gender_id')
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    confirmPassword = request.POST.get('confirm_password')
+
+    if password == confirmPassword:
+        encryptedPassword = make_password(password)
+        User.objects.create(first_name=firstName, middle_name=midddleName, last_name=lastName, age=age, birth_date=birthDate, gender_id=genderId, username=username, password=encryptedPassword)
+        
+        messages.success(request, 'User successfully')
+
+        return redirect('/users')
+    else:
+        messages.error(request, 'Password do no match')
+        return redirect('/user/create')
